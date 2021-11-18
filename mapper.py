3ffa1b262dc05813955cddfb9898e26f9a7d60bf
xhailey <panhailey2017@gmail.com>
2021-11-18 11:51:33 -0800

    update mapper

--- mapper.py
+++ mapper.py
@@ -4,15 +4,18 @@ for line in sys.stdin:
   line = line.strip()
   yearMonthDay = line[15:23]
   temperature = line[87:92] 
-  quality = line[93]
+  quality = line[92]
+  
+  try:
+    temperature = int(temperature)
+    quality = int(quality)
+  except ValueError:
+    continue
 
-    try:
-        temperature = int(temperature)
-        quality = int(quality)
-    except ValueError:
-        continue
-
-  if temperature == 9999 or quality == 0 or quality == 1 or quality == 4 or quality = 5 or quality = 9:
+  if temperature == 9999:
+    continue
+  
+  if quality != 0 and quality != 1 and quality != 4 and quality != 5 and quality != 9:
     continue
 
   print ('%s\t%d' % (yearMonthDay, temperature))
